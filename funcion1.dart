972aec63f9917c9d012fb0eacb71a062140c392d
Azul-Torres-1108 <167554917+Azul-Torres-1108@users.noreply.github.com>
2025-02-07 16:47:38 +0000

    mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,18 @@
+// funcion que no devuelve valor.
+// funcion saludar
+void saludar(){
+    print("hola mundo");
+}
+
+void escribeNombre(){
+    print("Mederyth Azul Torres");
+}
+void main(){
+    // Mederyth Azul Torres: 22308051281108
+    // Llamar a la funcion saludar
+    print("Mederyth Azul Torres: 22308051281108");
+    print("Llamando a la funcion saludar");
+    saludar();
+    print("Llamando a la funcion escribeNombre");
+    escribeNombre();    
+}
