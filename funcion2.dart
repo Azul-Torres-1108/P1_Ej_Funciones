7f1a254114f03a777c01ed2c723a9e4274e18ccf
Azul-Torres-1108 <167554917+Azul-Torres-1108@users.noreply.github.com>
2025-02-10 18:04:22 +0000

    ejemplo funcion rama

---
+++ funcion2.dart
@@ -0,0 +1,13 @@
+void suma (int a, int b){
+    print("La suma de $a + $b es: ${a+b}");
+}
+void resta (int a, int b){
+    print("La resta de $a - $b es: ${a-b}");
+}
+// funcion main 
+ void main() {
+    print("Mederyth Azul Torres: 22308051281108");
+    print("Llamando a la funcion suma y resta");
+    suma(5, 3);
+    resta(5, 3);
+ }
